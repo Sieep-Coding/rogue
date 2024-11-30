05c1aa70387807888efce1914976415ca4ab3646
sieep-coding <nstamb6@gmail.com>
2024-11-29 20:38:58 -0500

    sword for our hero!

--- src/entities/Player.cpp
+++ src/entities/Player.cpp
@@ -7,7 +7,7 @@ void Player::Update() {
     if (IsKeyDown(KEY_W)) y -= speed;
     if (IsKeyDown(KEY_S)) y += speed;
     if (IsKeyDown(KEY_A)) x -= speed;
-    if (IsKeyDown(KEY_D)) x -= speed;
+    if (IsKeyDown(KEY_D)) x += speed;
 
     if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
         //TODO: add sword swing
--- src/entities/Player.h
+++ src/entities/Player.h
@@ -1,3 +1,4 @@
+#pragma once
 #ifndef PLAYER_H
 #define PLAYER_H
 
@@ -6,10 +7,18 @@
 
 class Player : public Entity
 {
+private:
+    float swordAngle = 0.0f;
+    bool isSwinging = false;
+    float swingTimer = 0.0f;
+
 public:
     Player(float x, float y);
     void Update();
     void Draw();
+
+    bool IsSwinging() const { return isSwinging; }
+    float GetSwordAngle() const { return swordAngle; }
 };
 
 #endif
