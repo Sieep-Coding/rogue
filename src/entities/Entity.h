877b4e93c07323d650e765b9a6fd1f9dd4acaf71
sieep-coding <nstamb6@gmail.com>
2024-11-29 19:52:23 -0500

    added entity and player objects for position/velocity

---
+++ src/entities/Entity.h
@@ -0,0 +1,15 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
+class Entity {
+protected:
+    float x, y;
+    float speed;
+
+public:
+    Entity(float x, float y, float speed);
+    virtual void Update() = 0;
+    virtual void Draw() = 0;
+};
+
+#endif
